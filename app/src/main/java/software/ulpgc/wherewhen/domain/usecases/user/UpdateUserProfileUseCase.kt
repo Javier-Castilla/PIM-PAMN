8c72b073ae2b4248241323af6df77ee184285dc8
Javier-Castilla <javier.castilla101@alu.ulpgc.es>
2025-11-19 11:52:10 +0000

    feat: domain update user use case implemented

--- app/src/main/java/software/ulpgc/wherewhen/domain/usecases/user/UpdateUserProfileUseCase.kt
+++ app/src/main/java/software/ulpgc/wherewhen/domain/usecases/user/UpdateUserProfileUseCase.kt
@@ -1,5 +1,28 @@
 package software.ulpgc.wherewhen.domain.usecases.user
 
-class UpdateUserProfileUseCase {
-    // TODO: Implement use case
+import software.ulpgc.wherewhen.domain.exceptions.user.UserNotFoundException
+import software.ulpgc.wherewhen.domain.model.User
+import software.ulpgc.wherewhen.domain.persistence.repositories.UserRepository
+import software.ulpgc.wherewhen.domain.valueObjects.Email
+import software.ulpgc.wherewhen.domain.valueObjects.UUID
+
+data class UpdateUserProfileDTO(
+    val name: String? = null,
+    val email: Email? = null
+) {}
+
+class UpdateUserProfileUseCase(private val repository: UserRepository) {
+
+    suspend operator fun invoke(uuid: UUID, dto: UpdateUserProfileDTO): Result<User> {
+        val user = repository.getWith(uuid).mapCatching { it ->
+            it ?: throw UserNotFoundException(uuid)
+        }.getOrElse { error ->
+            return Result.failure(error)
+        }
+        val updatedUser = user.copy(
+            name = dto.name ?: user.name,
+            email = dto.email ?: user.email
+        )
+        return repository.update(updatedUser)
+    }
 }
