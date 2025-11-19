3d073bde259ed2a50ff4a5e343f851c2fa8a2776
Javier-Castilla <javier.castilla101@alu.ulpgc.es>
2025-11-19 11:39:49 +0000

    feat: domain get user use case implemented

--- app/src/main/java/software/ulpgc/wherewhen/domain/exceptions/user/UserNotFoundException.kt
+++ app/src/main/java/software/ulpgc/wherewhen/domain/exceptions/user/UserNotFoundException.kt
@@ -1,5 +1,9 @@
 package software.ulpgc.wherewhen.domain.exceptions.user
 
+import software.ulpgc.wherewhen.domain.valueObjects.Email
 import software.ulpgc.wherewhen.domain.valueObjects.UUID
 
-class UserNotFoundException(uuid: UUID) : UserException("User not found $uuid");
+class UserNotFoundException : UserException {
+    constructor(uuid: UUID) : super("User not found: $uuid")
+    constructor(email: Email) : super("User not found: $email")
+}
--- app/src/main/java/software/ulpgc/wherewhen/domain/usecases/user/GetUserUserCase.kt
+++ app/src/main/java/software/ulpgc/wherewhen/domain/usecases/user/GetUserUserCase.kt
@@ -1,5 +1,22 @@
 package software.ulpgc.wherewhen.domain.usecases.user
 
-class GetUserUserCase {
-    // TODO: Implement use case
+import software.ulpgc.wherewhen.domain.exceptions.user.UserNotFoundException
+import software.ulpgc.wherewhen.domain.model.User
+import software.ulpgc.wherewhen.domain.persistence.repositories.UserRepository
+import software.ulpgc.wherewhen.domain.valueObjects.Email
+import software.ulpgc.wherewhen.domain.valueObjects.UUID
+
+class GetUserUseCase(private val repository: UserRepository) {
+
+    suspend operator fun invoke(uuid: UUID): Result<User> {
+        return repository.getWith(uuid).mapCatching { user ->
+            user ?: throw UserNotFoundException(uuid)
+        }
+    }
+
+    suspend operator fun invoke(email: Email): Result<User> {
+        return repository.getWith(email).mapCatching { user ->
+            user ?: throw UserNotFoundException(email)
+        }
+    }
 }
