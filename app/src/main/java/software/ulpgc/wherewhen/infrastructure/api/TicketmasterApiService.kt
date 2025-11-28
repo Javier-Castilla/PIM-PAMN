25c69414ead1db14d45f3925f2c43e0a13c2cd7a
Javier-Castilla <javier.castilla101@alu.ulpgc.es>
2025-11-28 16:17:22 +0000

    something for Aldo

--- app/src/main/java/software/ulpgc/wherewhen/infrastructure/api/TicketmasterApiService.kt
+++ app/src/main/java/software/ulpgc/wherewhen/infrastructure/api/TicketmasterApiService.kt
@@ -1,28 +1,2 @@
-package software.ulpgc.wherewhen.infrastructure.api
+package software.ulpgc.wherewhen.infrastructure.api 
 
-import retrofit2.http.GET
-import retrofit2.http.Query
-import software.ulpgc.wherewhen.infrastructure.api.models.TicketmasterResponse
-
-interface TicketmasterApiService {
-    @GET("discovery/v2/events.json")
-    suspend fun searchEvents(
-        @Query("apikey") apiKey: String,
-        @Query("latlong") latLong: String,
-        @Query("radius") radius: Int,
-        @Query("unit") unit: String = "km",
-        @Query("size") size: Int = 50,
-        @Query("sort") sort: String = "date,asc"
-    ): TicketmasterResponse
-    
-    @GET("discovery/v2/events.json")
-    suspend fun searchEventsByCategory(
-        @Query("apikey") apiKey: String,
-        @Query("latlong") latLong: String,
-        @Query("radius") radius: Int,
-        @Query("classificationName") category: String,
-        @Query("unit") unit: String = "km",
-        @Query("size") size: Int = 50,
-        @Query("sort") sort: String = "date,asc"
-    ): TicketmasterResponse
-}
